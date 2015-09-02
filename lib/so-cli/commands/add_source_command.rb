479112ca6ead693c317634b444da4f8a2cf75733
Michaël Fortin <fortinmike@irradiated.net>
2015-09-01 20:31:25 -0400

    AddSourceCommand skeleton.

--- lib/so-cli/commands/add_source_command.rb
+++ lib/so-cli/commands/add_source_command.rb
@@ -2,7 +2,25 @@ require_relative 'command'
 
 module SoCli
   class AddSourceCommand < Command
+    self.command = "add-source"
+    self.summary = "Add an action source."
+    
+    def self.options
+      [['<repo_url>', 'The url of a repository containing so-cli actions at its root.']].concat(super)
+    end
+    
+    def initialize(argv)
+      @url = argv.shift_argument
+      super
+    end
+
+    def validate!
+      super
+      help! "You must provide the URL to a git repository" unless @url
+    end
+    
     def run
+      puts "Adding source #{@url}"
     end
   end
 end
--- lib/so-cli/commands/commands.rb
+++ lib/so-cli/commands/commands.rb
@@ -1,1 +1,2 @@
 require_relative 'command.rb'
+require_relative 'add_source_command.rb'
